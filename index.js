edeb3ad418b8d6a213711622b5b7c4bd3f2cb9e0
HP <20060339@stu.omu.edu.tr>
2023-02-22 23:45:46 +0300

    add form

---
+++ index.js
@@ -0,0 +1,9 @@
+let text = document.getElementById("text")
+let buton = document.getElementById("buton")
+
+function myFunction() {
+
+  let text = document.getElementById("text").value
+  let paragraf = document.getElementById("paragraf").innerHTML = `Değer : ` + text
+
+}
