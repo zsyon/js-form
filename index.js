e068c940f00637c631183be3680cd17bf854a72c
HP <20060339@stu.omu.edu.tr>
2023-02-22 23:53:07 +0300

    add form

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-let text = document.getElementById("text")
-let buton = document.getElementById("buton")
 
 function myFunction() {
 
-  let text = document.getElementById("text").value
-  let paragraf = document.getElementById("paragraf").innerHTML = `Değer : ` + text
+  let text = document.getElementById("text");
+  document.getElementById("paragraf").innerHTML = (`Değer : ` + text );
 
 }
