4b09ffebb68d8af7d58368622511f09f4d784f8c
HP <20060339@stu.omu.edu.tr>
2023-02-23 22:21:33 +0300

    add form js

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function myFunction() {
   document.getElementById("paragraf").innerHTML = (`Değer : ` + text );
 
 }
+
+let formDOM = document.getElementById("form")
+formDOM.addEventListener("submit" , form)
+
+function form() {
+  let name = document.getElementById("userName")
+  let not = document.getElementById("not")
+}
+
+
+
+
+let notDOM = document.getElementById("not")
+
